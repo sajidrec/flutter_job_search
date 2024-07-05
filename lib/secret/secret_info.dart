577773ffaa68e76a2aa9956be49a1df634adbb60
MD. Sajid Hossain <sajidrec@gmail.com>
2024-07-05 20:55:24 +0600

    job search option functional

--- lib/secret/secret_info.dart
+++ lib/secret/secret_info.dart
@@ -1,7 +1,7 @@
 class SecretInfo {
   static dynamic getHeader() {
     return {
-      'x-rapidapi-key': 'd4a737194bmshe1b67a7220e166ep1e2b91jsn09f99f403ea1',
+      'x-rapidapi-key': 'your api key here',
       'x-rapidapi-host': 'jsearch.p.rapidapi.com'
     };
   }
